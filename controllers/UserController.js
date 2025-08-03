51acc91739d5f547d352c4aec2d87c1895148d3b
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-08-03 11:57:41 +0530

    password encryption

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,28 @@
 const UserModel = require("../models/UserModel");
+const bcrypt = require("bcrypt");
+
 const AddUser = async (req, res) => {
   try {
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+    req.body.password = hashedPassword;
     const AddedUser = await UserModel.create(req.body);
     res.status(201).json({
       message: "User added successfully",
       user: AddedUser,
+    });
+  } catch (err) {
+    res.status(500).json({     
+      message: err.message,
+    });
+  }
+};
+const GetAllUsers = async (req, res) => {
+  try {
+    const AllUsers = await UserModel.find();
+    res.status(200).json({
+      message: "All users fetched successfully",
+      users: AllUsers,
     });
   } catch (err) {
     res.status(500).json({
@@ -12,17 +30,4 @@ const AddUser = async (req, res) => {
     });
   }
 };
-const GetAllUsers = async (req, res) => {
-  try{
-   const AllUsers = await UserModel.find();
-   res.status(200).json({
-    message: "All users fetched successfully",
-    users: AllUsers,
-   })
-  }catch(err){
-    res.status(500).json({
-      message: err.message,
-    });
-  }
-}
-module.exports = { AddUser,GetAllUsers };
+module.exports = { AddUser, GetAllUsers };
