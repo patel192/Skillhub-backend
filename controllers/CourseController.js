7d8a5a55f00d19a5040f89f63fccdda1c98b1ee6
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-08-05 12:23:15 +0530

    course by id

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -25,5 +25,18 @@ const GetCourses = async (req,res)=>{
    })
   }
 }
+const CourseById = async (req,res) =>{
+  try{
+  const Course = await CourseModel.findById(req.params.id)
+  res.status(200).json({
+    message:"Course fetched Successfully",
+    data:Course
+  })
+  }catch(err){
+  res.status(500).json({
+    message:err.message
+  })
+  }
+}
 
-module.exports = { CreateCourse, GetCourses }
+module.exports = { CreateCourse, GetCourses ,CourseById}
--- routes/CourseRoutes.js
+++ routes/CourseRoutes.js
@@ -2,4 +2,5 @@ const route = require('express').Router()
 const CourseController = require("../controllers/CourseController")
 route.post("/course",CourseController.CreateCourse)
 route.get("/courses",CourseController.GetCourses)
+route.get("/course/:id",CourseController.CourseById)
 module.exports = route;
