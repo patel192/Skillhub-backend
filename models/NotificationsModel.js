6250b3b6d12ba52ccc1caa8971716acd2757a569
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-08-07 12:29:12 +0530

    Notification API

---
+++ models/NotificationsModel.js
@@ -0,0 +1,22 @@
+const Schema = require("mongoose").Schema;
+const NotificationsSchema = Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  message: {
+    type: String,
+    required: [true, "Message is required"],
+  },
+  type: {
+    type:String,
+    enum:["event", "feedback", "certificate", "achievement"],
+    required: [true, "Notification type is required"]
+  },
+  read:{
+    type: Boolean,
+    default: false,
+  }
+},{
+    timestamps: true
+});
