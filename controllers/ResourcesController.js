dfde4fee7bcad820eb1bd4d90aeab3664c93b9fd
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-08-10 20:15:20 +0530

    resources for the course

--- controllers/ResourcesController.js
+++ controllers/ResourcesController.js
@@ -1,17 +1,32 @@
-const ResourcesModel = require("../models/ResourcesModel")
+const ResourcesModel = require("../models/ResourcesModel");
 const AddResource = async (req, res) => {
-    try{
-        const AddedResource = await ResourcesModel.create(req.body)
-        res.status(200).json({
-            message: "Resource Added Successfully",
-            data: AddedResource
-        })
-    }catch(err){
-        res.status(500).json({
-            message: err.message || "Internal Server Error"
-        })
-    }
-}
+  try {
+    const AddedResource = await ResourcesModel.create(req.body);
+    res.status(200).json({
+      message: "Resource Added Successfully",
+      data: AddedResource,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message || "Internal Server Error",
+    });
+  }
+};
+const GetResourceByCourseId = async (req, res) => {
+  try {
+    const Resource = await ResourcesModel.find({
+      courseId: req.params.courseId,
+    });
+    res.status(200).json({
+      message: "Course Resource Found Successfully",
+      data: Resource,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
 module.exports = {
-    AddResource
-}
+  AddResource,GetResourceByCourseId
+};
--- routes/ResourceRoutes.js
+++ routes/ResourceRoutes.js
@@ -1,4 +1,5 @@
 const route = require("express").Router()
 const ResourcesController = require("../controllers/ResourcesController")
-route.post("/resource",ResourcesController.AddResource)
+route.post("/lesson",ResourcesController.AddResource)
+route.get("/lessons/:courseId",ResourcesController.GetResourceByCourseId)
 module.exports = route
