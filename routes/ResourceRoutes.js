000485db5d8741d25122575e4be81393890f6d88
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-08-12 14:39:05 +0530

    Route updation

--- routes/ResourceRoutes.js
+++ routes/ResourceRoutes.js
@@ -1,5 +1,5 @@
 const route = require("express").Router()
 const ResourcesController = require("../controllers/ResourcesController")
-route.post("/lesson",ResourcesController.AddResource)
+route.post("/lessons",ResourcesController.AddResource)
 route.get("/lessons/:courseId",ResourcesController.GetResourceByCourseId)
 module.exports = route
