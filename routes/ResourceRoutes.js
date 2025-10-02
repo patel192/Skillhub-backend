fb71e02dae17c98f18d7af8daa2188658c002b5a
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-10-02 19:33:42 +0530

    code correction

--- routes/ResourceRoutes.js
+++ routes/ResourceRoutes.js
@@ -1,6 +1,8 @@
-const route = require("express").Router()
-const ResourcesController = require("../controllers/ResourcesController")
-const authMiddleware = require("../middleware/authMiddleware")
-route.post("/lessons",authMiddleware.verifyToken,authMiddleware.isAdmin,ResourcesController.AddResource)
-route.get("/lessons/:courseId",ResourcesController.GetResourceByCourseId)
-module.exports = route
+const route = require("express").Router();
+const ResourcesController = require("../controllers/ResourcesController");
+
+// No token verification, no admin check
+route.post("/lessons", ResourcesController.AddResource);
+route.get("/lessons/:courseId", ResourcesController.GetResourceByCourseId);
+
+module.exports = route;
