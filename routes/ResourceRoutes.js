f772ed60321e6aac3d627c2946b1fc04806035d5
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-10-02 19:08:34 +0530

    resources access

--- routes/ResourceRoutes.js
+++ routes/ResourceRoutes.js
@@ -2,5 +2,5 @@ const route = require("express").Router()
 const ResourcesController = require("../controllers/ResourcesController")
 const authMiddleware = require("../middleware/authMiddleware")
 route.post("/lessons",authMiddleware.verifyToken,authMiddleware.isAdmin,ResourcesController.AddResource)
-route.get("/lessons/:courseId",authMiddleware.verifyToken,ResourcesController.GetResourceByCourseId)
+route.get("/lessons/:courseId",ResourcesController.GetResourceByCourseId)
 module.exports = route
