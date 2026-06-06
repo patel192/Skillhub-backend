9df2d4e4e485587acbd7783e98e82c450a13bcf6
Muhammad <patelmuhammad192@gmail.com>
2026-06-06 12:03:38 +0530

    fix: correct authentication middleware usage in upload-thumbnail route

--- routes/CourseRoutes.js
+++ routes/CourseRoutes.js
@@ -6,5 +6,5 @@ route.post("/course",authMiddleware.verifyToken,authMiddleware.isAdmin,CourseCon
 route.get("/courses",authMiddleware.verifyToken,CourseController.GetCourses)
 route.get("/course/:id",authMiddleware.verifyToken,CourseController.CourseById)
 route.patch("/course/:id",authMiddleware.verifyToken,authMiddleware.isAdmin,CourseController.UpdateCourse)
-route.post("/course/upload-thumbnail",authMiddleware,isAdmin,upload.single("thumbnail"),CourseController.UploadCourseThumbnail);
+route.post("/course/upload-thumbnail",authMiddleware.verifyToken,authMiddleware.isAdmin,upload.single("thumbnail"),CourseController.UploadCourseThumbnail);
 module.exports = route;
